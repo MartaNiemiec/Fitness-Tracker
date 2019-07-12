391240bc07755adf240fc1fbcfb435f4b5e8ddb0
Marta Niemiec <niemiec.marta@hotmail.com>
2019-07-12 18:07:08 +0200

    add Sidenav and Toolbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'fitness-tracker';
-}
+export class AppComponent {}
