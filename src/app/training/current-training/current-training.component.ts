6fae40a2cfc56c1cd6eee792a14d9473fcd8eed9
Marta Niemiec <niemiec.marta@hotmail.com>
2019-07-13 18:49:46 +0200

    add a cancel Dialog screen to stop training

--- src/app/training/current-training/current-training.component.ts
+++ src/app/training/current-training/current-training.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
+
+import { StopTrainingComponent } from './stop-training.component';
 
 @Component({
   selector: 'app-current-training',
@@ -9,7 +12,7 @@ export class CurrentTrainingComponent implements OnInit {
   progress = 0;
   timer: number;
 
-  constructor() {}
+  constructor(private dialog: MatDialog) {}
 
   ngOnInit() {
     this.timer = setInterval(() => {
@@ -22,5 +25,6 @@ export class CurrentTrainingComponent implements OnInit {
 
   onStop() {
     clearInterval(this.timer);
+    this.dialog.open(StopTrainingComponent);
   }
 }
