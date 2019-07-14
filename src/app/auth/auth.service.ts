69b573d1227ee9c19e68f9051fb336882b616af1
Marta Niemiec <niemiec.marta@hotmail.com>
2019-07-14 23:22:49 +0200

    routing, authentication, route protection

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,18 +1,24 @@
+import { Injectable } from '@angular/core';
+
 import { Subject } from 'rxjs';
 
 import { User } from './user.model';
 import { AuthData } from './auth.data.model';
+import { Router } from '@angular/router';
 
+@Injectable()
 export class AuthService {
   authChange = new Subject<boolean>();
   private user: User;
+
+  constructor(private router: Router) {}
 
   registerUser(authData: AuthData) {
     this.user = {
       email: authData.email,
       userId: Math.round(Math.random() * 10000).toString()
     };
-    this.authChange.next(true);
+    this.authSuccessfully();
   }
 
   login(authData: AuthData) {
@@ -20,12 +26,13 @@ export class AuthService {
       email: authData.email,
       userId: Math.round(Math.random() * 10000).toString()
     };
-    this.authChange.next(true);
+    this.authSuccessfully();
   }
 
-  logout(authData: AuthData) {
+  logout() {
     this.user = null;
     this.authChange.next(false);
+    this.router.navigate(['/login']);
   }
 
   getUser() {
@@ -34,5 +41,10 @@ export class AuthService {
 
   isAuth() {
     return this.user != null;
+  }
+
+  private authSuccessfully() {
+    this.authChange.next(true);
+    this.router.navigate(['/training']);
   }
 }
--- src/app/navigation/header/header.component.ts
+++ src/app/navigation/header/header.component.ts
@@ -34,6 +34,10 @@ export class HeaderComponent implements OnInit, OnDestroy {
     this.sidenavToggle.emit();
   }
 
+  onLogout() {
+    this.authService.logout();
+  }
+
   ngOnDestroy() {
     this.authSubscription.unsubscribe();
   }
