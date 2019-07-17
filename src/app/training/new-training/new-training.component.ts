24a1e28e0aa46a09eaebd2e9f1e8a72fc7129d7a
Marta Niemiec <niemiec.marta@hotmail.com>
2019-07-17 12:05:41 +0200

    add exercise data, inject and use training servise

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { TrainingService } from '../training.service';
+import { Exercise } from '../exercise.module';
 
 @Component({
   selector: 'app-new-training',
@@ -7,10 +9,13 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 })
 export class NewTrainingComponent implements OnInit {
   @Output() trainingStart = new EventEmitter<void>();
+  exercises: Exercise[] = [];
 
-  constructor() {}
+  constructor(private trainingService: TrainingService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.exercises = this.trainingService.getAvailableExercises();
+  }
 
   onStartTraining() {
     this.trainingStart.emit();
---
+++ src/app/training/training.service.ts
@@ -0,0 +1,14 @@
+import { Exercise } from './exercise.module';
+
+export class TrainingService {
+  private availableExercises: Exercise[] = [
+    { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
+    { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
+    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
+    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
+  ];
+
+  getAvailableExercises() {
+    return this.availableExercises.slice(); //slice creates a real copy of exercises array; creates a new array
+  }
+}
